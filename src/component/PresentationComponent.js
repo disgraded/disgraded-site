8bf5ced2811c3e11ba18b080ea9aba997b1a55ef
Daniel Kalevski <kalevski@outlook.com>
2019-04-16 16:14:42 +0200

    added background animation (waves)

--- src/component/PresentationComponent.js
+++ src/component/PresentationComponent.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default ({ children }) => (
     <div className="presentation">
         <div className="presentation-background">
-            
+            <div className="wave-animation">
+                <div className="wave wave-top">
+                    <div className="wave-bg wave-bg-top"></div>
+                </div>
+                <div className="wave wave-middle">
+                    <div className="wave-bg wave-bg-middle"></div>
+                </div>
+                <div className="wave wave-bottom">
+                    <div className="wave-bg wave-bg-bottom"></div>
+                </div>
+            </div>
         </div>
         <div className="presentation-content">
             {children}

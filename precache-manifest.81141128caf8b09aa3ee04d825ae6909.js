4aa995ac57483042f0112b95ee63bb09596ce0b7
Daniel <kalevski@outlook.com>
2019-04-21 19:41:21 +0200

    Updates

---
+++ precache-manifest.81141128caf8b09aa3ee04d825ae6909.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6452be42dbc33a4624ed",
+    "url": "/static/js/main.abfa874d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6b8a369da03761f350b6",
+    "url": "/static/js/2.6ccb3ddb.chunk.js"
+  },
+  {
+    "revision": "ab88ca03b3265cdcdb55d77d37f55a2c",
+    "url": "/index.html"
+  }
+];

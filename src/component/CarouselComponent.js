c9245e23938df99507a01d9e140d8e8451ff99b5
Daniel <kalevski@outlook.com>
2019-04-23 13:44:38 +0200

    carousel problem with height of the content fixed

--- src/component/CarouselComponent.js
+++ src/component/CarouselComponent.js
@@ -1,22 +1,26 @@
 import React from 'react'
 
-export default ({ children, bg = 'black' }) => (
-    <div className="carousel">
-        <div className={'carousel-content carousel-content-color-' + bg}>
-            {children}
-        </div>    
-    <div className="carousel-animation">
-            <div className="wave-animation">
-                <div className="wave wave-top">
-                    <div className="wave-bg wave-bg-top"></div>
-                </div>
-                <div className="wave wave-middle">
-                    <div className="wave-bg wave-bg-middle"></div>
-                </div>
-                <div className="wave wave-bottom">
-                    <div className="wave-bg wave-bg-bottom"></div>
-                </div>
-            </div>
-        </div>
+let WaveDecoration = () => (
+    <div className="decoration wave-animation">
+        <div className="wave wave-top"><div className="wave-bg wave-bg-top"></div> </div>
+        <div className="wave wave-middle"><div className="wave-bg wave-bg-middle"></div></div>
+        <div className="wave wave-bottom"><div className="wave-bg wave-bg-bottom"></div></div>
     </div>
 )
+
+export default ({ children, bg = 'black', decoration = 'wave' }) => {
+    let decorationContent = ''
+    if (decoration === 'wave') {
+        decorationContent = <WaveDecoration />
+    }
+    return (
+        <div className="carousel">
+            <div className={'carousel-content carousel-content-color-' + bg}>
+                {children}
+            </div>    
+        <div className="carousel-decoration">
+                {decorationContent}
+            </div>
+        </div>
+    )
+}

0f2a3e104498f1cd0c36257ed29502d523027945
Daniel Kalevski <kalevski@outlook.com>
2019-04-24 19:16:46 +0200

    Added better description about QuariQ

--- src/component/QueriQLayersComponent.js
+++ src/component/QueriQLayersComponent.js
@@ -6,7 +6,7 @@ export default () => {
             <div className="row">
                 <div className="col-md-6">
                     <h2>QuariQ</h2>
-                    <p>Lightweight jvm based game engine for developing 2D games on Android, Widnows, Linux</p>
+                    <p>Game engine based on JVM platform with support for Android, Windows & Linux operating systems. Extensible and friendly for any type of developer. QuariQ is writen in Kotlin but our plan is to support JavaScript & Lua as a scripting languages. <br/> <b>Alpha release [v1.0.0]: 01.01.2020</b><br/> <b>Latest release [N/A]</b></p>
                     <button type="button" class="btn btn-primary">Download</button>
                     <button type="button" class="btn btn-secondary">Docs</button>
                     <button type="button" class="btn btn-secondary">Dev Log</button>

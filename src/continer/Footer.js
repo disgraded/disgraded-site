beaf5a48336bf8df0004ea6aa5fe5de883123774
Daniel <kalevski@outlook.com>
2019-04-25 18:12:41 +0200

    implemented footer of the site

---
+++ src/continer/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+
+    render() {
+        return (
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="footer-text">
+                                DisGraded | 2019
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        )
+    }
+}
--- src/page/landingPage.js
+++ src/page/landingPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from '../continer/Navigation'
 import LandingPane from '../continer/LandingPane'
-import LandingInfo from '../continer/LandingInfo';
+import LandingInfo from '../continer/LandingInfo'
+import Footer from '../continer/Footer'
 
 export default () => (
     <div className="page">
         <Navigation />
         <LandingPane />
         <LandingInfo />
+        <Footer />
     </div>
 )
